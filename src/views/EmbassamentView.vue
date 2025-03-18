a1da851dcf2b788b0aaa10b495d456dcc54f979e
Hugo <hugoalonso343@gmail.com>
2025-03-18 20:01:19 +0100

    goes

--- src/views/EmbassamentView.vue
+++ src/views/EmbassamentView.vue
@@ -1,12 +1,110 @@
 <script setup>
-let props = defineProps()
-console.log(props.value)
+import { useRoute } from 'vue-router';
+import { ref } from 'vue'
+import axios from 'axios'
+
+const route = useRoute();
+const embassament = route.params.embassament;
+let capacidadVolumen = 0;
+let capacidadPorcentaje = 0;
+let capacidadTotal = 0;
+console.log(embassament);
+let embassamentUrl = embassament.replaceAll(" ", '%20');
+
+let url = `https://analisi.transparenciacatalunya.cat/resource/gn9e-3qhr.json?$query=SELECT%20%60dia%60%2C%60estaci%60%2C%60nivell_absolut%60%2C%60percentatge_volum_embassat%60%2C%60volum_embassat%60%20WHERE%20%60estaci%60%20=%20%27${embassamentUrl}%27%20ORDER%20BY%20%60dia%60%20DESC%20LIMIT%20100`
+let dades = ref([])
+const data = ref([])
+
+axios
+  .get(url)
+  .then((response) => {
+    data.value = response.data
+    for (let i in data.value) {
+      if (!dades.value.includes(data.value[i].estaci)) {
+        console.log('yes')
+
+        let fecha = new Date(data.value[i].dia);
+        let mes = getMonth(fecha.getMonth())
+        let fechaString = `${fecha.getDate()} de ${mes} de ${fecha.getFullYear()}`;
+        capacidadVolumen+=parseInt(data.value[i].volum_embassat);
+        capacidadPorcentaje+=parseInt(data.value[i].percentatge_volum_embassat);
+        let objeto = {
+          "dia": fechaString,
+          "estaci": embassament,
+          "nivell_absolut":data.value[i].nivell_absolut,
+          "percentatge_volum_embassat": data.value[i].percentatge_volum_embassat,
+          "volum_embassat": data.value[i].volum_embassat
+        }
+        dades.value.push(objeto)
+      }
+    }
+    console.log(dades.value)
+    capacidadTotal = (capacidadVolumen*100)/capacidadPorcentaje;
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+function getMonth(num){
+  let mes = "";
+  switch(num){
+    case 0:
+      mes = "Gener";
+      break;
+    case 1:
+      mes = "Febrer";
+      break;
+    case 2:
+      mes = "Març";
+      break;
+    case 3:
+      mes = "Abril";
+      break;
+    case 4:
+      mes = "Maig";
+      break;
+    case 5:
+      mes = "Juny";
+      break;
+    case 6:
+      mes = "Juliol";
+      break;
+    case 7:
+      mes = "Agost";
+      break;
+    case 8:
+      mes = "Setembre";
+      break;
+    case 9:
+      mes = "Octubre";
+      break;
+    case 10:
+      mes = "Novembre";
+      break;
+    case 11:
+      mes = "Desembre";
+      break;
+  }
+  return mes
+}
 </script>
 
 <template>
-<h2>Embassament view</h2>
+<h2>{{ embassament }}</h2>
+<h3>Capacidad general: {{ Math.round(capacidadTotal*100)/100 }} hm3</h3>
+<div class="day" v-for="(dada,index) in dades" :key="index">
+  <h3>Dia {{index+1}}: {{ dada.dia }}</h3>
+  <p>Nivell Absolut: {{ dada.nivell_absolut }}</p>
+  <p>Porcentaje de volumen embasado: {{ dada.percentatge_volum_embassat }} % </p>
+  <p>Volumen embasado: {{ dada.volum_embassat }}</p>
+</div>
 </template>
 
 <style>
-
+.day{
+  border: 1px solid black;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 10px;
+}
 </style>
--- src/views/WaterView.vue
+++ src/views/WaterView.vue
@@ -36,52 +36,5 @@ axios
 </template>
 
 <style>
-.title {
-  font-family: 'Arial', sans-serif;
-  text-align: center;
-  font-size: 2.5rem;
-  color: #4caf50;
-  margin-bottom: 30px;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-}
 
-.embassament-list {
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  max-width: 600px;
-  margin: 0 auto;
-  background: linear-gradient(135deg, #00c6ff, #0072ff); /* Azul */
-  border-radius: 15px;
-  padding: 20px;
-}
-
-.embassament-item {
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 15px;
-  font-size: 18px;
-  font-weight: bold;
-  color: #333;
-  text-align: center;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08);
-  transition: transform 0.3s, box-shadow 0.3s, background-color 0.3s;
-  cursor: pointer;
-}
-
-.embassament-item:hover {
-  transform: translateY(-5px);
-  box-shadow: 0 12px 20px rgba(0, 0, 0, 0.15), 0 6px 12px rgba(0, 0, 0, 0.1);
-  background-color: #0072ff; /* Azul */
-  color: #fff;
-}
-
-.embassament-item:active {
-  transform: translateY(2px);
-  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.1), 0 2px 6px rgba(0, 0, 0, 0.05);
-}
 </style>
